283ad5915309d63b5b81d3a7df871b01f1ebd0a9
Bart <bartosz.szucko@gmail.com>
2021-09-13 20:54:29 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Project of my own portfolio website.
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './styles/index.scss'
+
+function App() {
+  return (
+    <>
+      <p>Portfolio</p>
+    </>
+  )
+}
+
+export default App
