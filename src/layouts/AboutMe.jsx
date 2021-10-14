88107207bb7b1376f81a31c5207dc72dfe6676a8
Bart <bartosz.szucko@gmail.com>
2021-10-14 22:23:13 +0200

    update and add media aboutMePage

--- src/layouts/AboutMe.jsx
+++ src/layouts/AboutMe.jsx
@@ -4,7 +4,15 @@ import Hamburger from '../component/Hamburger'
 
 import PolishCv from '../../public/files/Bartosz_Szucko_CV_PL.pdf'
 import EnglishCv from '../../public/files/Bartosz_Szucko_CV_EN.pdf'
-import photo from '../../public/images/photos/myPhotoS.jpg'
+import heroPhotoXs from '../../public/images/photos/myPhotoXs.jpg'
+import heroPhotoSw from '../../public/images/photos/myPhotoSw.jpg'
+import heroPhotoS from '../../public/images/photos/myPhotoS.jpg'
+import heroPhotoM from '../../public/images/photos/myPhotoM.jpg'
+import heroPhotoL from '../../public/images/photos/myPhotoL.jpg'
+import heroPhotoXl from '../../public/images/photos/myPhotoXl.jpg'
+import heroPhotoXw from '../../public/images/photos/myPhotoXw.jpg'
+import heroPhotoXlw from '../../public/images/photos/myPhotoXlw.jpg'
+import heroPhotoXll from '../../public/images/photos/myPhotoXll.jpg'
 
 const AboutMe = () => {
   const pageVariants = {
@@ -30,8 +38,8 @@ const AboutMe = () => {
 
   const hamburgerVariants = {
     exit: {
-      y: -500,
-      x: 500,
+      y: -1000,
+      x: 1000,
       scale: 0.8,
       opacity: 0,
       transition: { type: 'tween', duration: 0.5 },
@@ -56,7 +64,39 @@ const AboutMe = () => {
 
       <div className='aboutMe__info'>
         <picture className='aboutMe__info-photo'>
-          <img src={photo} alt='photo' />
+          <source
+            media='(orientation: portrait) and (min-width: 1024px)'
+            srcSet={heroPhotoXll}
+          />
+          <source
+            media='(orientation: landscape) and (min-width:1440px)'
+            srcSet={heroPhotoXlw}
+          />
+          <source
+            media='(orientation: portrait) and (min-width:768px)'
+            srcSet={heroPhotoXl}
+          />
+          <source
+            media='(orientation: landscape) and (min-width:1024px)'
+            srcSet={heroPhotoXw}
+          />
+          <source
+            media='(orientation: portrait) and (min-width:400px)'
+            srcSet={heroPhotoL}
+          />
+          <source
+            media='(orientation: portrait) and (min-width:375px)'
+            srcSet={heroPhotoM}
+          />
+          <source
+            media='(orientation: portrait) and (min-width:360px)'
+            srcSet={heroPhotoS}
+          />
+          <source
+            media='(orientation: landscape) and (min-width:600px)'
+            srcSet={heroPhotoSw}
+          />
+          <img src={heroPhotoXs} alt='heroPhoto' />
         </picture>
         <p className='aboutMe__info-text1'>
           I am a self taught developer. I learned to program from online
