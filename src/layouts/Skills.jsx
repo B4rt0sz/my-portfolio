287921a199dec6c4fdc7e5ac2d72ccb020001193
Bart <bartosz.szucko@gmail.com>
2021-10-14 22:23:45 +0200

    update and add media skillsPage

--- src/layouts/Skills.jsx
+++ src/layouts/Skills.jsx
@@ -2,34 +2,60 @@ import { motion } from 'framer-motion'
 
 import Hamburger from '../component/Hamburger'
 
-import htmlImg from '../../public/images/icons/html48.png'
-import cssImg from '../../public/images/icons/css48.png'
-import sassImg from '../../public/images/icons/sass48.png'
-import jsImg from '../../public/images/icons/js48.png'
-import reactImg from '../../public/images/icons/react48.png'
-import reduxImg from '../../public/images/icons/redux48.png'
-import tsImg from '../../public/images/icons/ts48.png'
-import bsImg from '../../public/images/icons/bs48.png'
-import nodeImg from '../../public/images/icons/node48.png'
-import expressImg from '../../public/images/icons/express48.png'
-import mongoImg from '../../public/images/icons/mongo48.png'
-import webpackImg from '../../public/images/icons/webpack48.png'
-import gitImg from '../../public/images/icons/git48.png'
+import htmlImg from '../../public/images/icons/48/html48.png'
+import htmlImgM from '../../public/images/icons/64/html64.png'
+import htmlImgL from '../../public/images/icons/96/html96.png'
+import cssImg from '../../public/images/icons/48/css48.png'
+import cssImgM from '../../public/images/icons/64/css64.png'
+import cssImgL from '../../public/images/icons/96/css96.png'
+import sassImg from '../../public/images/icons/48/sass48.png'
+import sassImgM from '../../public/images/icons/64/sass64.png'
+import sassImgL from '../../public/images/icons/96/sass96.png'
+import jsImg from '../../public/images/icons/48/js48.png'
+import jsImgM from '../../public/images/icons/64/js64.png'
+import jsImgL from '../../public/images/icons/96/js96.png'
+import reactImg from '../../public/images/icons/48/react48.png'
+import reactImgM from '../../public/images/icons/64/react64.png'
+import reactImgL from '../../public/images/icons/96/react96.png'
+import reduxImg from '../../public/images/icons/48/redux48.png'
+import reduxImgM from '../../public/images/icons/64/redux64.png'
+import reduxImgL from '../../public/images/icons/96/redux96.png'
+import tsImg from '../../public/images/icons/48/ts48.png'
+import tsImgM from '../../public/images/icons/64/ts64.png'
+import tsImgL from '../../public/images/icons/96/ts96.png'
+import bsImg from '../../public/images/icons/48/bs48.png'
+import bsImgM from '../../public/images/icons/64/bs64.png'
+import bsImgL from '../../public/images/icons/96/bs96.png'
+import nodeImg from '../../public/images/icons/48/node48.png'
+import nodeImgM from '../../public/images/icons/64/node64.png'
+import nodeImgL from '../../public/images/icons/96/node96.png'
+import expressImg from '../../public/images/icons/48/express48.png'
+import expressImgM from '../../public/images/icons/64/express64.png'
+import expressImgL from '../../public/images/icons/96/express96.png'
+import mongoImg from '../../public/images/icons/48/mongo48.png'
+import mongoImgM from '../../public/images/icons/64/mongo64.png'
+import mongoImgL from '../../public/images/icons/96/mongo96.png'
+import webpackImg from '../../public/images/icons/48/webpack48.png'
+import webpackImgM from '../../public/images/icons/64/webpack64.png'
+import webpackImgL from '../../public/images/icons/96/webpack96.png'
+import gitImg from '../../public/images/icons/48/git48.png'
+import gitImgM from '../../public/images/icons/64/git64.png'
+import gitImgL from '../../public/images/icons/96/git96.png'
 
 const skillsList = [
-  { text: 'HTML', src: htmlImg },
-  { text: 'CSS', src: cssImg },
-  { text: 'SASS', src: sassImg },
-  { text: 'JavaScript', src: jsImg },
-  { text: 'React', src: reactImg },
-  { text: 'Redux', src: reduxImg },
-  { text: 'TypeScript', src: tsImg },
-  { text: 'Bootstrap', src: bsImg },
-  { text: 'Node', src: nodeImg },
-  { text: 'Express', src: expressImg },
-  { text: 'MongoDB', src: mongoImg },
-  { text: 'Webpack', src: webpackImg },
-  { text: 'Git', src: gitImg },
+  { text: 'HTML', srcS: htmlImg, srcM: htmlImgM, srcL: htmlImgL },
+  { text: 'CSS', srcS: cssImg, srcM: cssImgM, srcL: cssImgL },
+  { text: 'SASS', srcS: sassImg, srcM: sassImgM, srcL: sassImgL },
+  { text: 'JavaScript', srcS: jsImg, srcM: jsImgM, srcL: jsImgL },
+  { text: 'React', srcS: reactImg, srcM: reactImgM, srcL: reactImgL },
+  { text: 'Redux', srcS: reduxImg, srcM: reduxImgM, srcL: reduxImgL },
+  { text: 'TypeScript', srcS: tsImg, srcM: tsImgM, srcL: tsImgL },
+  { text: 'Bootstrap', srcS: bsImg, srcM: bsImgM, srcL: bsImgL },
+  { text: 'Node', srcS: nodeImg, srcM: nodeImgM, srcL: nodeImgL },
+  { text: 'Express', srcS: expressImg, srcM: expressImgM, srcL: expressImgL },
+  { text: 'MongoDB', srcS: mongoImg, srcM: mongoImgM, srcL: mongoImgL },
+  { text: 'Webpack', srcS: webpackImg, srcM: webpackImgM, srcL: webpackImgL },
+  { text: 'Git', srcS: gitImg, srcM: gitImgM, srcL: gitImgL },
 ]
 
 const Skills = () => {
@@ -56,8 +82,8 @@ const Skills = () => {
 
   const hamburgerVariants = {
     exit: {
-      y: -500,
-      x: 500,
+      y: -1000,
+      x: 1000,
       scale: 0.8,
       opacity: 0,
       transition: { type: 'tween', duration: 0.5 },
@@ -66,7 +92,21 @@ const Skills = () => {
 
   const skillList = skillsList.map((icon) => (
     <div className='skills__section-list-item' key={icon.text}>
-      <img src={icon.src} alt={icon.text} />
+      <picture>
+        <source
+          media='(orientation: landscape) and (min-width:1400px) and (min-height:1300px)'
+          srcSet={icon.srcL}
+        />
+        <source
+          media='(orientation: portrait) and (min-width:768px)'
+          srcSet={icon.srcM}
+        />
+        <source
+          media='(orientation: landscape) and (min-width:1200px)'
+          srcSet={icon.srcM}
+        />
+        <img src={icon.srcS} alt={icon.text} />
+      </picture>
       <p>{icon.text}</p>
     </div>
   ))
